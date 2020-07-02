2fee3ffd215383f8733b4969ac70c39464ab1d24
Spalding Vance <spaldingvance@Spaldings-MacBook-Pro.local>
2020-07-02 10:29:28 -0400

    commit before heroku

--- server/client/src/reducers/index.js
+++ server/client/src/reducers/index.js
@@ -2,11 +2,15 @@ import { combineReducers } from "redux";
 import MoviesReducer from "./reducer-movies";
 import MoviesOrderReducer from "./reducer-movies-order";
 import TotalPagesReducer from "./reducer-total-pages";
+import AuthReducer from "./reducer-auth";
+import { reducer as formReducer } from "redux-form";
 
 const rootReducer = combineReducers({
   movies: MoviesReducer,
   movies_order: MoviesOrderReducer,
-  total_pages: TotalPagesReducer
+  total_pages: TotalPagesReducer,
+  form: formReducer,
+  auth: AuthReducer,
 });
 
 export default rootReducer;
